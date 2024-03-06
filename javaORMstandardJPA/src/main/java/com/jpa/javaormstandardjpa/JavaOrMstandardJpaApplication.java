b8b80a03c97941e95dce73a798e164cd064638b1
ji-jjang <jinyhehe@gmail.com>
2024-03-07 02:20:32 +0900

    feat: delete excercise

--- javaORMstandardJPA/src/main/java/com/jpa/javaormstandardjpa/JavaOrMstandardJpaApplication.java
+++ javaORMstandardJPA/src/main/java/com/jpa/javaormstandardjpa/JavaOrMstandardJpaApplication.java
@@ -1,12 +1,7 @@
 package com.jpa.javaormstandardjpa;
 
-import com.jpa.javaormstandardjpa.domain.Member;
-import jakarta.annotation.PostConstruct;
-import jakarta.persistence.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
