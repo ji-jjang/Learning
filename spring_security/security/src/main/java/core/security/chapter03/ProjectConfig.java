ec9fe68973f37f0aa9e8330e39105d62139b365f
ji-jjang <jinyhehe@gmail.com>
2024-03-21 14:43:22 +0900

    feat: JdbcUserDetailsManager 구현에 이용되는 쿼리 변경하기

--- spring_security/security/src/main/java/core/security/chapter03/ProjectConfig.java
+++ spring_security/security/src/main/java/core/security/chapter03/ProjectConfig.java
@@ -16,7 +16,13 @@ public class ProjectConfig {
 
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
+        String usersByUsernameQuery = "select username, password, enabled from users where username = ?";
+        String authsByUserQuery = "select username, authority from spring.authorities where username = ?";
+
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+        return userDetailsManager;
     }
 
     @Bean
