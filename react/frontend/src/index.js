a6330ccfc24d00af9768c39cc77df6cde4072dd5
ji-jjang <jinyhehe@gmail.com>
2024-03-30 21:23:17 +0900

    feat: frontend router 적용

--- react/frontend/src/index.js
+++ react/frontend/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react'; // 리액트 사용을 위해 import
-import ReactDOM from 'react-dom/client'; // ㅣㄹ액트 돔을 사용하기 위해
+import { createRoot } from 'react-dom/client'; 
 import './index.css'; // css import
-import App from './App'; // App 컴포넌트 import
-
+import AppRouter from './AppRouter';
 import reportWebVitals from './reportWebVitals'; 
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // ReactDOM이 내부의 컴포넌트들을 'root' 엘리먼트에 render함.
+const root = createRoot(document.getElementById('root'));
+
+// 루트에 AppRouter 컴포넌트 렌더링
 root.render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
