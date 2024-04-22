53f458a75eb4278e27783a493c448e0bb37e7e66
jinyus <jinyhehe@gmail.com>
2024-04-22 22:39:36 +0900

    feat: java 프로젝트

---
+++ Practice/EngineerInformationProcessing/src/test/java/com/core/engineerinformationprocessing/EngineerInformationProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.engineerinformationprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineerInformationProcessingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
