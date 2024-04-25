4e2ecb845006082334d6db8f91fa4c1d3a85a22d
jinyus <jinyhehe@gmail.com>
2024-04-25 13:22:25 +0900

    feat: strategy pattern

---
+++ Book/DesignPatternIntroduction_HIROSHI_YUKI/src/main/java/com/example/designpatternintroduction_hiroshi_yuki/J10_strategy/a1/Strategy.java
@@ -0,0 +1,7 @@
+package com.example.designpatternintroduction_hiroshi_yuki.J10_strategy.a1;
+
+public interface Strategy {
+  public abstract Hand nextHand();
+
+  public abstract void study(boolean win);
+}
