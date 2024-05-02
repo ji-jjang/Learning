28cb5b8c81d15cd81e7f7cc784cea7fd751efed5
jinyus <jinyhehe@gmail.com>
2024-05-02 09:44:05 +0900

    feat: chapter01(eventListener)

--- Book/SpringBootInPractice_솜나트무시브/src/main/java/com/juny/core/CoreApplication.java
+++ Book/SpringBootInPractice_솜나트무시브/src/main/java/com/juny/core/CoreApplication.java
@@ -1,13 +1,25 @@
 package com.juny.core;
 
+import com.juny.core.ch01.ApplicationStartingEventListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class CoreApplication {
 
 	public static void main(String[] args) {
+		SpringApplication springApplication = new SpringApplication(CoreApplication.class);
+		springApplication.addListeners(new ApplicationStartingEventListener());
 		SpringApplication.run(CoreApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void applicationReadyEvent(ApplicationReadyEvent applicationReadyEvent) {
+		System.out.println("Application Ready Event generated at " + new Date(applicationReadyEvent.getTimestamp()));
 	}
 
 }
