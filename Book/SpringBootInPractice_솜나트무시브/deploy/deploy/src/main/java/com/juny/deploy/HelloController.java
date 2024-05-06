15c66205ae35c09830a03110265b42319eb9d9bd
jinyus <jinyhehe@gmail.com>
2024-05-06 20:44:17 +0900

    feat: deploy

---
+++ Book/SpringBootInPractice_솜나트무시브/deploy/deploy/src/main/java/com/juny/deploy/HelloController.java
@@ -0,0 +1,16 @@
+package com.juny.deploy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/hello")
+  public String hello(@RequestParam String name) {
+
+    return "hello " + name;
+  }
+
+}
