8aeb19c82f860f2f370b547c04378f0891b2a3fd
jinyus <jinyhehe@gmail.com>
2024-09-26 15:36:15 +0900

    Load Balancer

---
+++ Practice/load_balancer/spring2/src/test/java/com/juny/loadbalancertest/LoadBalancerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juny.loadbalancertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalancerTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
---
+++ Practice/load_balancer/spring3/src/main/java/com/juny/loadbalancertest/LoadBalancerTestApplication.java
@@ -0,0 +1,13 @@
+package com.juny.loadbalancertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadBalancerTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LoadBalancerTestApplication.class, args);
+  }
+
+}
